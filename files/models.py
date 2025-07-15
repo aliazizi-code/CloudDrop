fc6485ad192e11997d265faed1674724c7449bf8
aliazizi-code <devaliazizi@gmail.com>
2025-07-15 11:55:01 +0330

    delete class uploadfile model

--- files/models.py
+++ files/models.py
@@ -1,13 +1,1 @@
 from django.db import models
-
-
-class UploadFile(models.Model):
-    file = models.FileField(upload_to='uploads/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.file.name
-
-    class Meta:
-        verbose_name = 'Uploaded File'
-        verbose_name_plural = 'Uploaded Files'
